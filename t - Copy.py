b9ec33ac9fe671ccb505debf2a62ad582d5ce9ab
ledduh <ledduh@gmail.com>
2017-05-08 15:27:45 +0300

    copy of coffee program

---
+++ t - Copy.py
@@ -0,0 +1,19 @@
+def coffee():
+	people=[]
+	for x in people:
+		print ('1. Took a cup')
+		print ('2. Poured coffee into a cup')
+		print ('3. Added sugar')
+		print ('4. Pour Milk')
+		print ('6. Serve')
+coffee()
+
+def serve_coffee(name):
+	coffee()
+	print ("Here is your tasty coffee " +  name + " Enjoy")
+	
+serve_coffee('Evans')
+serve_coffee('Naomi')
+
+
+
